78c60c5ef1a526b927e10d83f7be03928abce8ff
sergei <kho.sergey@gmail.com>
2017-02-03 21:37:25 -0500

    caching + new lookups work!

--- dns.js
+++ dns.js
@@ -3,9 +3,9 @@ const http = require("http");
 const fs = require("fs");
 const port = 1302;
 
-/*
+/******************************************************************************
 *Dns lookup function
-*/
+*******************************************************************************/
 function dnsLookUp(url,cb){
 	dns.lookup(url,function (err,addresses){
 				if(err !== null){
@@ -18,10 +18,14 @@ function dnsLookUp(url,cb){
 }
 
 
+
+/*********************************************************************************
+*looks up dns and creates file
+**********************************************************************************/
 function createDnsFile(url,cb){
 	dnsLookUp(url,function(address){			
 		var data = url +" "+ address+"\n";
-		fs.writeFile(__dirname+'/dns-Cache.txt',data, function (err)  {
+		fs.writeFile(__dirname+'/dns-cache.txt',data, function (err)  {
 			if (err !== null){
 				console.log("[ERROR]: " + err);
 			}
@@ -33,30 +37,32 @@ function createDnsFile(url,cb){
 }
 
 
-/*
+/*************************************************************************************
 *Checks for file existance
 *Checks if dns lookup already exists
-*/
+**************************************************************************************/
 function checkInFile(url,cb){
-	fs.readFile(__dirname+'/dns-Cache.txt', function (err, data)  {
-		var dataString = data.toString('utf-8');
-		
+	fs.readFile(__dirname+'/dns-cache.txt', function (err, data)  { 
+
+		//file not found handling
 		if(err !== null &&  err.code === "ENOENT"){
 			process.nextTick(function(){return cb(false)});		
 		}
+
+		//other errors
 		else if(err !== null &&  err.code !== "ENOENT"){
 			console.log(err);		
 		}
-		else if(dataString.match(url)){ 
+
+		else if(data.toString('utf-8').match(url)){ 
 			//find the url and return the number next to it.
-			console.log("found it");
 			process.nextTick(function(){return cb(1)});
 		}
 		else{
 			dnsLookUp(url,function(address){	
 					process.nextTick(function(){return cb(address)});
 					//append to file
-					fs.appendFile(__dirname+'/dns-Cache.txt', address, function(err){
+					fs.appendFile(__dirname+'/dns-cache.txt', url+" "+address+"\n", function(err){
  					 if (err){
 						console.log(err);
 					 }
@@ -67,43 +73,54 @@ function checkInFile(url,cb){
 	});
 }
 
-/*
-* return dns and url 
-*/
-/***************************************************************************FIX MATCHING LINE AND RETURNING
+
+
+/**************************************************************************
+* return dns and url  from file
+**************************************************************************/
 function findLine(url,cb){
-	fs.readFile(__dirname+'/dns-Cache.txt', function (err, data)  {
+	fs.readFile(__dirname+'/dns-cache.txt', function (err, data)  {
 	if(err !== null){
 			console.log(err);		
 	}
 	else{
 		var dataString = data.toString('utf-8');
-		var pattern = ".*"+url+".*";
-		var r = new Regex(pattern);
-		for (that string matching)
-		{
-				 process.nextTick(function(){return cb(m.Value)});
+		var splitLines = dataString.split("\n");
+		splitLines.pop();
+
+		
+		for(var i = 0; i < splitLines.length; i++){
+			if(splitLines[i].match(url) ){
+				var data = splitLines[i];
+				process.nextTick(function(){return cb(data)});
+			}
 		}
+		
 	}
 });
 }
-********************************************************************************/
+
+
+
+/*******************************************************************************
+* Server 
+*******************************************************************************/
 http.createServer(function(request, response){
 	const url = request.url.substring(1,request.url.length);
 	
 	//check in dns cached file
 	checkInFile(url, function(data){
-		if(data === false){//then file is not found, make it, perform dns lookup,save it send it to client
-			createDnsFile(url,function(address){
+		if(data === false){
+			createDnsFile(url,function(addressFromLookUp){
 			response.writeHead(200, {"Content-Type": "text/html"});		
-			response.write(address);
+			response.write(addressFromLookUp);
 			response.end();
 			});
 		}
 		else if(data === 1){
-			findLine(url,function(address){
+			findLine(url,function(addressFromFile){
 			response.writeHead(200, {"Content-Type": "text/html"});		
-			response.write(address);
+			response.write(addressFromFile);
 			response.end();
 			});
 		}
